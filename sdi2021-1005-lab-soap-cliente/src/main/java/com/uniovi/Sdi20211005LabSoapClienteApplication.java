eeb9b71036a63c1cd39a99c0dca9c8684b5a05cb
samuelmorenov <xamelsamuel@gmail.com>
2021-04-25 20:50:58 +0200

    SDI-2021-1005-SW-11.2- Servicio SOAP Cliente

---
+++ sdi2021-1005-lab-soap-cliente/src/main/java/com/uniovi/Sdi20211005LabSoapClienteApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi20211005LabSoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi20211005LabSoapClienteApplication.class, args);
+	}
+
+}
